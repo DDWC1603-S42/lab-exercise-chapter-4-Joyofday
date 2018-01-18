803239f3d148016debd28c8a12ee72acd50dcb68
Dayang Nur Atiqah <33915604+Joyofday@users.noreply.github.com>
2018-01-18 09:20:14 +0800

    I don't know??

--- Lab-Exercise-3.cpp
+++ Lab-Exercise-3.cpp
@@ -1,15 +1,23 @@
-//Display the number in reverse order
-//fix and complete the program below.
+//Name:Dayang Nur Atiqah bt. Ag Zulkarenain
+//Matric no.:A17DW1171
 
+#include<iostream>
+using namespace std;
 
-cout<<"user input a number: ";
-
-for(t=num;num!=0;num=num/10)
-{
-	r=num % 10;
-	sum=sum*10+r;
-
-}
-cout<<"The number in reverse order: "<<endl;
-
+int main() {
+	
+	int t,r,sum,num;
+	
+	cout<<"Enter an integer: "<<endl;
+	cin>>t;
+	
+	for(t=num;num!=0;num=num/10)
+	{
+		r=num%10;
+		sum=sum*10+r;
+	}
+	
+	cout<<"Reversed number: "<<sum;
+	
+	return 0;
 }

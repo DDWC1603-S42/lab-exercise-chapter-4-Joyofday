e5d2534475550c33d69fce9dabfd2d9c005374a6
Dayang Nur Atiqah <33915604+Joyofday@users.noreply.github.com>
2018-01-18 09:05:03 +0800

    Done

--- Lab-Exercise-2.cpp
+++ Lab-Exercise-2.cpp
@@ -1,33 +1,37 @@
-//Check whether a number is prime or not
-//finish up this program, properly, and nicely.
+//Name:Dayang Nur Atiqah bt. Ag Zulkarenain
+//Matric no.:A17DW1171
 
-#include <iostream>
+#include<iostream>
+#include<cctype>
 using namespace std;
 
-int main()
-{
-
+int main() {
+	
 	int i=1,u=1,sum=0;
 	cout<<"The perfect numbers between 1 to 500 are: "<<endl;
-
-	while(i<=500)
-
-		while(u<=500)
-
+	
+	while(i<500)
+	{
+		while(u<500)
+		{
 			if(u<i)
-
-				if(i%u=)
+			{
+				if(i%u==0)
+				{
 					sum=sum+u;
-
-	u++
-
-		if(sum=i)
-			
-			cout<<i<<" "<<endl;
-
-	i++
-		u=1;
-		sum=0;
-
-
+					u++;
+				}
+			}
+		}
+	}
+	
+	if(sum==i)
+		{
+			cout<<i<<"is a perfect number."<<endl;
+			i++;
+			u=1;
+			sum=0;
+		}
+		
+	return 0;
 }

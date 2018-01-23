808ed79dc7df4742acffa675a777f2612b0be58e
Dayang Nur Atiqah <33915604+Joyofday@users.noreply.github.com>
2018-01-23 23:11:25 +0800

    Check mark

--- Lab-Exercise-5.cpp
+++ Lab-Exercise-5.cpp
@@ -1,19 +1,22 @@
 //Get out from this program
 //find a way to fix and complete this program. good luck.
+//Dayang Nur Atiqah binti Ag Zulkarenain
+//A17DW1171
 
 #include <iostream>
 using namespace std;
 int main()
 {
-	while(false)//infinite loop
+	while(true)//infinite loop
 	{
 		std::cout<<"Enter 0 to exit or anyhing else to continue: ";
 		int num;
 		cin>>num;	
 
 		//exit loop if user enters
-		if(num==0)
+		if(num==0) {
 			break;
+		}
 	}
 	std::cout<<"we are out";
 
